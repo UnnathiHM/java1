402418cc3d89e49d777f0b5caaa2cfef6d12c792
UnnathiHM <unnathi2hm.xworkz@gmail.com>
2021-10-05 20:09:48 +0530

    removefile

--- hardDisk.java
+++
@@ -1,9 +0,0 @@
-class hardDisk{
-	int y;
-	hardDisk(int y)
-	{ System.out.println(y);
-		 y=10;
-		 System.out.println(y);
-		
-	}
-}
--- motor.java
+++
@@ -1,8 +0,0 @@
-class motor{
-	
-	motor(int x,int y)
-	{ 
-		System.out.println("x"+x+"y"+y);
-
-	}
-}
