be8d22b83672239adc695fe3aea61de56484d30f
UnnathiHM <unnathi2hm.xworkz@gmail.com>
2021-10-05 22:11:06 +0530

    removefile

--- speaker.java
+++
@@ -1,8 +0,0 @@
-class speaker{
-	public static void company(){
-		System.out.println("speaker company name:JBL");
-	}
-	public static void type(){
-		System.out.println("Type of Speaker:Soundbars");
-	}
-	}
--- speakerRunner.java
+++
@@ -1,9 +0,0 @@
-class speakerRunner{
-	public static void main(String[] b){
-		System.out.println("----Calling method inside the speaker function---");
-		speaker.company();
-		speaker.type();
-	
-	}
-	
-}
