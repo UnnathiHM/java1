caa9df25815bd42d00abca8c847124c8a1995edf
UnnathiHM <unnathi2hm.xworkz@gmail.com>
2021-10-05 22:30:50 +0530

    project

---
+++ PrintMonth.java
@@ -0,0 +1,35 @@
+class  PrintMonth{
+	public static void main(String[] arg){
+		String jan="JANUARY";
+		String feb="FEBRUARY";
+		String mar="MARCH";
+		String  aprl="APRIL";
+		String may="MAY";
+		String jun="JUNE";
+		String jul="JULY";
+		String aug="AUGUST";
+		String sep="SPETEMBER";
+		String oct="OCTOBER";
+		String nov="NOVEMBER";
+		String dec="DECEMBER";
+		
+		String[] listMonth={jan,feb,mar,aprl,may,jun,jul,aug,sep,oct,nov,dec};
+		System.out.println("PRINT MONTH");
+		System.out.println("1."+ listMonth[0]);
+		System.out.println("2."+ listMonth[1]);
+		System.out.println("3."+ listMonth[2]);
+		System.out.println("4."+ listMonth[3]);
+		System.out.println("5."+ listMonth[4]);
+		System.out.println("6."+ listMonth[5]);
+		System.out.println("7."+ listMonth[6]);
+		System.out.println("8."+ listMonth[7]);
+		System.out.println("8."+ listMonth[8]);
+		System.out.println("10."+ listMonth[9]);
+		System.out.println("11."+ listMonth[10]);
+		System.out.println("12."+ listMonth[11]);
+		
+		
+		
+		
+	}
+}
